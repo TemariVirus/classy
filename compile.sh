261ead5a7a509df7fc76b9c39c2f44013b99c5e9
TemariVirus <kiyonnxperiaz@gmail.com>
2025-11-04 12:42:09 +0800

    Use zig as C compiler

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euox pipefail
 
-cc -std=c17 -Wall -Wextra -Wpedantic -o classy.o src/main.c $@
+zig cc -std=c17 -Wall -Wextra -Wpedantic -o classy.o src/main.c $@
--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -euox pipefail
 
-cc -std=c17 -Wall -Wextra -Wpedantic -o run-tests.o tests/main.c
+zig cc -std=c17 -Wall -Wextra -Wpedantic -o run-tests.o tests/main.c
 ./run-tests.o
