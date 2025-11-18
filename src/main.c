bca68a380ffe19a9babd52adef793ad2de45539f
TemariVirus <kiyonnxperiaz@gmail.com>
2025-11-18 23:50:49 +0800

    Add startup message

--- src/main.c
+++ src/main.c
@@ -1,48 +1,73 @@
+#include <stdbool.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
-#include "db.h"
-#include "error.h"
+#if defined(_WIN32)
+#include <windows.h>
+#endif
+
+#define LINE_BUF_SIZE 4096
+#define USER_NAME "P1_1"
+#define SYSTEM_NAME "CMS"
+
+void print_startup_message(void) {
+    // Message to guide new user
+    fprintf(stdout, "Welcome to Classy, a class management system!\n");
+    fprintf(stdout, "Type a single line command here, and press <Enter> to run it.\n");
+    fprintf(stdout, "Exit Classy by pressing ctrl+c.\n");
+    fprintf(stdout, "For help on command syntax, run the \"HELP\" command.\n");
+}
+
+void print_prompt(const char* prompt_name) { fprintf(stdout, "%s: ", prompt_name); }
+
+// Reads a line from stdin into buf of size 'size'.
+// Returns buf on success, NULL if the line is too long.
+char* get_line(char* buf, size_t size) {
+    if (fgets(buf, size, stdin) == NULL) {
+        // EOF or error
+        return NULL;
+    }
+
+    // Ensure the whole line fits in the buffer
+    size_t newline_offset = strcspn(buf, "\n");
+    if (buf[newline_offset] == '\n') {
+        // Remove newline character
+        buf[newline_offset] = '\0';
+        return buf;
+    }
+
+    // Line too long
+    if (newline_offset >= size - 1) {
+        fprintf(stdout, "Input line too long. Maximum length is %zu characters.\n", size - 2);
+        // Clear the rest of the line from stdin
+        for (int c = fgetc(stdin); c != '\n' && c != EOF;) {
+            c = fgetc(stdin);
+        }
+    }
+    return NULL;
+}
 
 int main(void) {
-    DB db;
-    FILE* fptr = fopen("sample-db.txt", "rb");
-    if (fptr == NULL) {
-        perror("Failed to open file");
-        return EXIT_FAILURE;
-    }
-    DB_from_file__Error err = DB_from_file(fptr, &db);
-    fclose(fptr);
-    switch (err) {
-    case ERROR_OK:
-        break;
-    case ERROR_MISSING_TABLE_NAME:
-        printf("Error: Missing table name in file.\n");
-        break;
-    case ERROR_BAD_FORMAT:
-        printf("Error: Badly formatted file.\n");
-        break;
-    case ERROR_BAD_COLUMN:
-        printf("Error: Column name must be one of ID, Name, Programme, or Mark and cannot contain "
-               "duplicates.\n");
-        break;
-    case ERROR_UNORDERED_ID:
-        printf("Error: IDs in file are not in ascending order.\n");
-        break;
+#if defined(_WIN32)
+    // Needed on Windows to print utf-8 to the terminal
+    SetConsoleOutputCP(65001);
+#endif
+    print_startup_message();
+
+    char line_buf[LINE_BUF_SIZE];
+    while (!feof(stdin)) {
+        print_prompt(USER_NAME);
+        char* line = get_line(line_buf, LINE_BUF_SIZE);
+        if (line == NULL) {
+            continue;
+        }
+
+        print_prompt(SYSTEM_NAME);
+        fprintf(stdout, "len: %zu \n", strlen(line));
     }
 
-    printf("Table Name: %s\n", db.table_name);
-    printf("Row Count: %zu\n", db.row_count);
-
-    ID id;
-    Row* row;
-    TTreeIter it = TTree_iter_start(&db.data);
-    while (TTree_iter_next(&it, &id, &row)) {
-        printf("%10i: name=%s\t programme=%s\t mark=%f\n", id, row->name, row->programme,
-               row->mark);
-    }
-
-    DB_destroy(&db);
-
+    fprintf(stdout, "\n");
+    print_prompt(SYSTEM_NAME);
+    fprintf(stdout, "Exiting Classy. Goodbye!\n");
     return 0;
 }
