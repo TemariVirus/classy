b4679c64d7ede49b741e1ed5da881d6e5d8557fe
TEMARI VÍRUS <kiyonnxperiaz@gmail.com>
2025-11-23 11:58:57 +0000

    Implement Condition_eval

--- src/condition.c
+++ src/condition.c
@@ -5,6 +5,7 @@
 
 #include "row.c"
 #include "tokenizer.c"
+#include "unreachable.c"
 
 struct Condition;
 // Strings in this union are references to the original unparsed string, not copies.
@@ -61,6 +62,47 @@ void Condition_destroy(Condition* cond) {
     }
 }
 
+// Returns the value of the specified column of the given row.
+// The data type of `column` must be VALUE_INT.
+static uint32_t __get_int_column(ID id, const Row* row, Column column) {
+    (void)row;
+    switch (column) {
+    case COLUMN_ID:
+        return id;
+    case COLUMN_NAME:
+    case COLUMN_PROGRAMME:
+    case COLUMN_MARK:
+        UNREACHABLE;
+    }
+}
+
+// Returns the value of the specified column of the given row.
+// The data type of `column` must be VALUE_FLOAT.
+static float __get_float_column(const Row* row, Column column) {
+    switch (column) {
+    case COLUMN_MARK:
+        return row->mark;
+    case COLUMN_ID:
+    case COLUMN_NAME:
+    case COLUMN_PROGRAMME:
+        UNREACHABLE;
+    }
+}
+
+// Returns the value of the specified column of the given row.
+// The data type of `column` must be VALUE_STRING.
+static char* __get_string_column(const Row* row, Column column) {
+    switch (column) {
+    case COLUMN_NAME:
+        return row->name;
+    case COLUMN_PROGRAMME:
+        return row->programme;
+    case COLUMN_ID:
+    case COLUMN_MARK:
+        UNREACHABLE;
+    }
+}
+
 // Returns whether the given ID and Row fulfill `cond`.
 // Returns true if `cond` is NULL.
 bool Condition_eval(const Condition* cond, ID id, const Row* row) {
@@ -68,8 +110,53 @@ bool Condition_eval(const Condition* cond, ID id, const Row* row) {
         return true;
     }
 
-    // TODO
-    (void)id;
-    (void)row;
-    return true;
+    switch (cond->tag) {
+    case OP_OR:
+        return Condition_eval(cond->args.and_or.lhs, id, row) ||
+               Condition_eval(cond->args.and_or.rhs, id, row);
+    case OP_AND:
+        return Condition_eval(cond->args.and_or.lhs, id, row) &&
+               Condition_eval(cond->args.and_or.rhs, id, row);
+    case OP_NOT:
+        return !Condition_eval(cond->args.not.cond, id, row);
+    case OP_IN:
+        return strstr(__get_string_column(row, cond->args.in.column), cond->args.in.value) != NULL;
+    case OP_LT:
+        switch (Column_type(cond->args.eq_gt_lt.column)) {
+        case VALUE_INT:
+            return __get_int_column(id, row, cond->args.eq_gt_lt.column) <
+                   cond->args.eq_gt_lt.value.ui;
+        case VALUE_FLOAT:
+            return __get_float_column(row, cond->args.eq_gt_lt.column) <
+                   cond->args.eq_gt_lt.value.f;
+        case VALUE_STRING:
+            UNREACHABLE;
+        }
+    case OP_GT:
+        switch (Column_type(cond->args.eq_gt_lt.column)) {
+        case VALUE_INT:
+            return __get_int_column(id, row, cond->args.eq_gt_lt.column) >
+                   cond->args.eq_gt_lt.value.ui;
+        case VALUE_FLOAT:
+            return __get_float_column(row, cond->args.eq_gt_lt.column) >
+                   cond->args.eq_gt_lt.value.f;
+        case VALUE_STRING:
+            UNREACHABLE;
+        }
+    case OP_EQ:
+        switch (Column_type(cond->args.eq_gt_lt.column)) {
+        case VALUE_INT:
+            return __get_int_column(id, row, cond->args.eq_gt_lt.column) ==
+                   cond->args.eq_gt_lt.value.ui;
+        case VALUE_FLOAT:
+            return __get_float_column(row, cond->args.eq_gt_lt.column) ==
+                   cond->args.eq_gt_lt.value.f;
+        case VALUE_STRING:
+            return strcmp(__get_string_column(row, cond->args.eq_gt_lt.column),
+                          cond->args.eq_gt_lt.value.s) == 0;
+        }
+    case OP_RPAREN:
+    case OP_LPAREN:
+        UNREACHABLE;
+    }
 }
